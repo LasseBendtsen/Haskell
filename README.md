49356983c5720bcaafb2dc857dec18f32f034052
LasseBendtsen <aalummi@proton.me>
2025-07-12 18:35:33 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Haskell
- Flake for Haskell Development on NixOS
+# Haskell Development Environment
+
+This repository provides a simple Haskell development environment using Nix.
+
+## Usage
+
+Run the following command to enter the development shell:
+
+```bash
+nix develop
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Simple Haskell dev environment using ghcWithPackages";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        ghc = pkgs.haskellPackages.ghcWithPackages (hp: with hp; [
+          cabal-install
+          ghcid
+          haskell-language-server
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ ghc ];
+        };
+      }
+    );
+}
