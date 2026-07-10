3caefbc8fb697769b448034dcb2428222a200376
Aalummi <aalummi@proton.me>
2026-07-10 18:03:22 +0200

    Update nixpkgs URL to use unstable version

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "Simple Haskell dev environment using ghcWithPackages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -18,7 +19,8 @@
           ghcid
           haskell-language-server
         ]);
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = [ ghc ];
         };
